95b8ba93fb06b8a3b8edbd2838a6c28896b308d6
Ziga Strgar <ziga_strgar@hotmail.com>
2014-10-26 13:57:19 +0100

    New icons, some classes, first page

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
 <?php include_once 'header.php'; ?>
+<div class="block-flat col-lg-12">
+    <h3 class="page-header">Domača stran</h3>
+    <div class="row">
+        <div class="col-md-6 col-sm-12">
+            Del 1
+            <img src="" alt="Part image" width="300" height="300" />
+        </div>
+        <div class="col-md-6 col-sm-12">
+            Del 2
+            <img src="" alt="Part image" width="300" height="300" />
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6 col-sm-12">
+        <div class="col-lg-12 bg-warning" style="height: 50px; padding-top: 15px;">
+            <span class="icon icon-search-1 text-warning"></span> <span class="text-warning">Iščete določen avto del?</span> <a href="search.php" style="margin-top: -5px;" class="btn btn-flat btn-warning-inverse pull-right">Poišči del</a>
+        </div>
+    </div>
+    <div class="col-md-6 col-sm-12">
+        <div class="col-lg-12 bg-success" style="height: 50px; padding-top: 15px;">
+            <span class="icon icon-euro text-success"></span> <span class="text-success">Imate odvečen avto del?</span> <a href="registration.php" style="margin-top: -5px;" class="btn btn-flat btn-success-inverse pull-right">Pridruži se nam</a>
+        </div>
+    </div>
+</div>
 <?php include_once 'footer.php'; ?>

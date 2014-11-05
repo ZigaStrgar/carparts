79e653cdfd8a43c40b00a3272e1209a915428bce
Ziga Strgar <ziga_strgar@hotmail.com>
2014-11-05 09:16:13 +0100

    search with models

---
+++ fetchModels.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once './core/session.php';
+include_once './core/database.php';
+include_once './core/functions.php';
+$id = (int) cleanString($_POST["id"]);
+$queryModels = "SELECT * FROM models WHERE brand_id = $id";
+$resultModels = mysqli_query($link, $queryModels);
+echo "<div class=\"input-group\">
+                    <span class=\"input-group-addon\">Model</span>
+                    <select name=\"model\" class=\"form-control\">
+                        <option value=\"0\" selected=\"selected\"></option>
+                        ";
+while ($model = mysqli_fetch_array($resultModels)) {
+    echo "<option value='".$model["id"]."'>".$model["name"]."</option>";
+}
+echo "</select>
+                </div>";
+?>
--- search.php
+++ search.php
@@ -1,10 +1,10 @@
 <?php include_once 'header.php'; ?>
 <?php
 //TIPI
-$queryTypes = "SELECT * FROM types";
+$queryTypes = "SELECT * FROM types ORDER BY name ASC";
 $resultTypes = mysqli_query($link, $queryTypes);
 //ZNAMKE
-$queryBrands = "SELECT * FROM brands";
+$queryBrands = "SELECT * FROM brands ORDER BY name ASC";
 $resultBrands = mysqli_query($link, $queryBrands);
 //MODELI
 ?>
@@ -24,7 +24,7 @@ $resultBrands = mysqli_query($link, $queryBrands);
         </div>
         <h4 class="page-header">Podatki o avtomobilu</h4>
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <div class="input-group">
                     <span class="input-group-addon">Znamka</span>
                     <select name="brand" placeholder="Znamka" class="form-control">
@@ -35,23 +35,47 @@ $resultBrands = mysqli_query($link, $queryBrands);
                     </select>
                 </div>
             </div>
-            <div class="col-md-4">
+            <div id="model" class="col-md-6">
+
+            </div>
+        </div>
+        <h4 class="page-header">Podatki o delu</h4>
+        <div class="row">
+            <div class="col-md-6">
                 <div class="input-group">
-                    <span class="input-group-addon">Model</span>
-                    <select name="model" disabled="disabled" class="form-control">
-                        <option value="0" selected="selected"></option>
-                    </select>
+                    <span class="input-group-addon"><i>Kataloška #</i></span>
+                    <input type="text" name="number" class="form-control" />
                 </div>
             </div>
-            <div class="col-md-4">
-                <?php while ($type = mysqli_fetch_array($resultTypes)) { ?>
-                    <div class="input-group">
-                        <?php echo $type["name"]; ?>
-                        <input type="checkbox" style="margin: 0 10px 0 0;" name="types[]">
-                    </div>
-                <?php } ?>
+            <div class="col-md-6">
+                <div class="input-group">
+                    <span class="input-group-addon">Ime dela</span>
+                    <input type="text" name="number" class="form-control" />
+                </div>
+            </div>
+        </div>
+        <br />
+        <div class="row">
+            <div class="col-lg-12">
+                <input type="submit" class="btn btn-flat btn-success" value="Išči"/>
             </div>
         </div>
     </form>
 </div>
+<script>
+    $(document).on("change", "select[name=brand]", function () {
+        getModels($(this).val());
+    });
+
+    function getModels(id) {
+        $.ajax({
+            url: "fetchModels.php",
+            type: "POST",
+            data: {id: id},
+            success: function (cb) {
+                $("#model").html(cb);
+            }
+        });
+    }
+</script>
 <?php include_once 'footer.php'; ?>

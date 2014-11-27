800c2a26afe0fceb145533715508f4f4387df98e
Ziga Strgar <ziga_strgar@hotmail.com>
2014-11-27 08:17:24 +0100

    Part add

--- addPart.php
+++ addPart.php
@@ -19,13 +19,12 @@ $resultBrands = mysqli_query($link, $queryBrands);
 $queryCategories = "SELECT * FROM categories WHERE category_id = 0 ORDER BY name ASC";
 $resultCategories = mysqli_query($link, $queryCategories);
 ?>
-<div class="col-lg-12 block-flat">
-    <?php if(!empty($_SESSION["error"])){ ?>
+<?php if (!empty($_SESSION["error"])) { ?>
     <div class="alert alert-danger alert-fixed-bottom">
         <p>
-            <?php 
-            switch ($_SESSION["error"]){
-                case 1: 
+            <?php
+            switch ($_SESSION["error"]) {
+                case 1:
                     echo "Napaka podatkovne baze!";
                     break;
                 case 2:
@@ -34,123 +33,120 @@ $resultCategories = mysqli_query($link, $queryCategories);
                 case 3:
                     echo "Napaka podatkov! Nekatera polja niso bila izpolnjena!";
                     break;
-            } 
+            }
             unset($_SESSION["error"]);
             ?>
         </p>
     </div>
-    <?php } ?>
-    <h3 class="page-header">Dodajanje avto dela</h3>
-    <form action="addingPart.php" method="POST" role="form" enctype="multipart/form-data">
-        <h4 class="page-header">Tip avtomobila</h4>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="input-group">
-                    <?php while ($type = mysqli_fetch_array($resultTypes)) { ?>
-                        <?php echo $type["name"]; ?><input style="margin: 0 10px 0 5px;" type="radio" name="types" value="<?php echo $type["id"]; ?>" />
-                    <?php } ?>
-                </div>
-            </div>
-        </div>
-        <br />
-        <h4 class="page-header">Podatki o avtomobilu</h4>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="input-group">
-                    <span class="input-group-addon">Znamka</span>
-                    <select name="brand" placeholder="Znamka" class="form-control">
-                        <option value="0" selected="selected"></option>
-                        <?php while ($brand = mysqli_fetch_array($resultBrands)) { ?>
-                            <option value="<?php echo $brand["id"]; ?>"><?php echo $brand["name"]; ?></option>
-                        <?php } ?>
-                    </select>
-                </div>
-            </div>
-            <div id="model" class="col-md-6">
-
-            </div>
-        </div>
-        <br />
-        <div class="row">
-            <div class="col-md-6">
-                <div class="input-group">
-                    <span class="input-group-addon">Tip</span>
-                    <input type="text" name="type" class="form-control" />
-                </div>
-            </div> 
-            <div class="col-md-6">
-                <div class="input-group">
-                    <span class="input-group-addon">Letnik</span>
-                    <input type="text" name="letnik" pattern="[0-9]{4}" title="Primer: 2014" class="form-control" />
-                </div>
-            </div>    
-        </div>
-        <h4 class="page-header">Podatki o delu</h4>
-        <div class="row">
-            <div class="col-md-6 col-xs-12">
-                <div class="input-group">
-                    <span class="input-group-addon">Kataloška številka</span>
-                    <input type="text" name="number" class="form-control" placeholder="Vnesi kataloško številko dela" />
-                </div>
-            </div>
-            <div class="col-md-6 col-xs-12">
-                <div class="input-group">
-                    <span class="input-group-addon">Ime dela</span>
-                    <input type="text" name="name" class="form-control" placeholder="Vnesi ime dela" />
-                </div>
-            </div>
-        </div>
-        <br />
-        <div class="row">
-            <div class="col-md-6 col-xs-12">
-                <div class="input-group">
-                    <span class="input-group-addon">Cena</span>
-                    <input type="text" name="price" class="form-control" title="1-5,0-2 številk" placeholder="Cena dela">
-                    <span class="input-group-addon"><i class="icon icon-euro"></i></span>
-                </div>
-            </div>
-            <div class="col-xs-12 col-md-6">
-                <?php if (mysqli_num_rows($resultCategories) > 0) { ?>
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-tags"></i></span>
-                        <select name="category" class="form-control">
-                            <option selected="selected"></option>
-                            <?php while ($category = mysqli_fetch_array($resultCategories)) { ?>
-                                <option value="<?php echo $category["id"]; ?>"><?php echo $category["name"] ?></option>
-                            <?php } ?>
-                        </select>
+<?php } ?>
+<div id="formType">
+    <div class="col-lg-12 block-flat">
+        <h3 class="page-header">Vnesti želim</h3>
+        <div class="product-chooser"> 
+            <?php
+            while ($category = mysqli_fetch_array($resultCategories)) {
+                ?>
+                <div class="col-lg-12" style="width: 185px;">
+                    <div class="product-chooser-item">
+                        <center><img src="<?php echo $category["image"]; ?>" alt="Category image" /></center>
+                        <div class="col-lg-12">
+                            <input type="radio" name="type" value="<?php echo $category["id"]; ?>">
+                        </div>
+                        <div class="clear"></div>
                     </div>
-                <?php } ?>
-            </div>
-        </div>
-        <br />
-        <div id="otherCategories" class="row">
-
-        </div>
-        <br />
-        <div class="row">
-            <div class="col-md-12">
-                <div class="input-group">
-                    <span class="input-group-addon">Opis dela</span>
-                    <textarea name="description" class="form-control" placeholder="Opis dela"></textarea>
+                    <center><span class="description"><div style="border-radius: 0px; padding: 5px 10px; font-size: 14px;" class="badge" id="badge<?php echo $category["id"]; ?>"><?php echo $category["name"]; ?></div></span></center>
                 </div>
-            </div>
+                <?php
+            }
+            ?>
         </div>
-        <br />
-        <div class="row">
-            <div class="col-md-12">
-                <div class="fileinput fileinput-new input-group" data-provides="fileinput">
-        <div class="form-control" data-trigger="fileinput"><i class="glyphicon glyphicon-picture fileinput-exists"></i> <span class="fileinput-filename"></span></div>
-        <span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new">Izberi sliko</span><span class="fileinput-exists">Spremeni sliko</span><input name="image" accept="image/*" type="file"></span>
-        <a class="input-group-addon btn btn-default fileinput-exists" href="#" data-dismiss="fileinput">Odstrani sliko</a>
-      </div>
-            </div>
-        </div>
-        <br />
-        <input type="hidden" name="cat" />
-        <input type="submit" name="submit" class="btn btn-flat btn-success" value="Dodaj del"/>
-    </form>
+    </div>
 </div>
+<div id="formLoad">
+
+</div>
+<script type="text/javascript">
+    $(function () {
+        $('div.product-chooser').not('.disabled').find('div.product-chooser-item').on('click', function () {
+            $('div.product-chooser-item').removeClass('selected');
+            $(this).addClass('selected');
+            $(this).find('input[type="radio"]').prop("checked", true);
+        });
+    });
+</script>
+<style>
+    .thumbnail {
+        position:relative;
+        overflow:hidden;
+    }
+
+    .caption {
+        position:absolute;
+        top:0;
+        right:0;
+        background:rgba(71, 143, 209, 0.90);
+        width:100%;
+        height:100%;
+        padding:2%;
+        display: none;
+        text-align:center;
+        color:#fff !important;
+        z-index:2;
+    }
+
+    div.clear
+    {
+        clear: both;
+    }
+
+    div.product-chooser.disabled div.product-chooser-item
+    {
+        zoom: 1;
+        filter: alpha(opacity=60);
+        opacity: 0.6;
+        cursor: default;
+    }
+
+    div.product-chooser div.product-chooser-item{
+        padding: 11px;
+        cursor: pointer;
+        position: relative;
+        border: 1px solid #efefef;
+        margin-bottom: 10px;
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    div.product-chooser div.product-chooser-item.selected{
+        border: 4px solid #5cb85c;
+        background: #fff;
+        padding: 8px;
+        filter: alpha(opacity=100);
+        opacity: 1;
+    }
+
+    div.product-chooser div.product-chooser-item img{
+        padding: 0;
+    }
+
+    div.product-chooser div.product-chooser-item span.title{
+        display: block;
+        margin: 10px 0 5px 0;
+        font-weight: bold;
+        font-size: 12px;
+    }
+
+    div.product-chooser div.product-chooser-item span.description{
+        font-size: 12px;
+    }
+
+    div.product-chooser div.product-chooser-item input{
+        position: absolute;
+        left: 0;
+        top: 0;
+        visibility:hidden;
+    }
+</style>
 <script src="./plugins/autosize/jquery.autosize.min.js"></script>
 <script>
     function fetchCategories(id) {
@@ -193,5 +189,23 @@ $resultCategories = mysqli_query($link, $queryCategories);
             }
         });
     }
+
+    $(document).on("click", "#back", function () {
+        location.reload();
+    });
+
+    $(document).on("click", "div.product-chooser-item", function () {
+        $id = $(this).find("input[type=radio]").attr("value");
+        $value = $("#badge"+$id).text();
+        $.ajax({
+            url: "formLoad.php",
+            type: "POST",
+            data: {id: $id, value: $value},
+            success: function (cb) {
+                $("#formLoad").html(cb);
+                $("#formType").hide();
+            }
+        });
+    });
 </script>
 <?php include_once 'footer.php'; ?>

0de96ff46033362b24c765edf3def7da5d5a8cb6
Žiga Strgar <ziga_strgar@hotmail.com>
2015-05-20 09:33:57 +0200

    error documents

---
+++ 404-second.php
@@ -0,0 +1,17 @@
+<?php include_once 'header.php'; ?>
+<div class='col-lg-offset-3 col-lg-6 col-md-12' style="margin-top: 5%;">
+    <center>
+        <h1>Ooops, nekaj je šlo narobe!</h1>
+        <br />
+        <br />
+        <img src="http://<?php echo URL; ?>/img/404.png" alt="Error 404 image" width="284" />
+        <br />
+        <br />
+        <h4>Stran, ki jo iščeš ne obstaja, je bila preimenovana, izbrisana ali pa mogoče sploh ni obstajala!</h4>
+        <br />
+        <a href="mailto:ziga_strgar@hotmail.com"><i style="font-size: 30px;" class="icon icon-letter-mail"></i></a>
+        <br />
+        <a href="<?php echo $_SERVER["HTTP_REFERER"]; ?>" class="btn btn-flat btn-primary"><i class="icon icon-arrow-left"></i> Pojdi nazaj</a>
+    </center>
+</div>
+<?php include_once 'footer.php'; ?>
